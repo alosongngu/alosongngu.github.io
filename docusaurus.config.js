49046f7416dacca492c82074331e1a5e8d0f9d83
Alo Song Ngữ <alosongngu@gmail.com>
2024-08-15 15:16:00 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'alosongngu', // Usually your GitHub org/user name.
   projectName: 'alosongngu.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
