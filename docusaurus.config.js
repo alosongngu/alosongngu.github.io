3363b7861760fb532992ab8ccd67ca693858c441
Alo Song Ngữ <alosongngu@gmail.com>
2024-08-16 16:52:22 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,20 +8,20 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'ePlus News',
-  tagline: 'Dinosaurs are cool',
+  title: 'Alo Song Ngữ News',
+  tagline: 'Giải pháp học ngoại ngữ trực tuyến',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://news.eplus.dev',
+  url: 'https://alosongngu.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ePlus-DEV', // Usually your GitHub org/user name.
-  projectName: 'news', // Usually your repo name.
+  organizationName: 'alosongngu', // Usually your GitHub org/user name.
+  projectName: 'alosongngu.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -46,14 +46,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ePlus-DEV/news/edit/main/',
+            'https://github.com/alosongngu/alosongngu.github.io/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ePlus-DEV/news/edit/main/',
+            'https://github.com/alosongngu/alosongngu.github.io/edit/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -74,9 +74,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'ePlus News',
+        title: 'Alo Song Ngữ News',
         logo: {
-          alt: 'ePlus News',
+          alt: 'Alo Song Ngữ News',
           src: 'img/logo.svg',
         },
         items: [
@@ -88,7 +88,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/ePlus-DEV/news',
+            href: 'https://github.com/alosongngu/alosongngu.github.io',
             label: 'GitHub',
             position: 'right',
           },
@@ -101,8 +101,8 @@ const config = {
             title: 'Thông tin',
             items: [
               {
-                label: 'ePlus.DEV',
-                to: 'https://eplus.dev',
+                label: 'Alo Song Ngữ',
+                to: 'https://alosongngu.com',
               },
             ],
           },
@@ -110,12 +110,12 @@ const config = {
             title: 'Mạng xã hội',
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/gwFtTJHM',
+                label: 'Facebook',
+                href: 'https://facebook.com/alosongngu',
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/david_nguyen94',
+                href: 'https://twitter.com/alosongngu',
               },
             ],
           },
@@ -128,12 +128,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/ePlus-DEV/news',
+                href: 'https://github.com/alosongngu/alosongngu.github.io',
               },
             ],
           },
         ],
-        copyright: `© 2019-${new Date().getFullYear()}. ePlus.DEV Labs điều hành & phát triển.`,
+        copyright: `Bản quyền © 2019-${new Date().getFullYear()} · Alo Song Ngữ · Chịu trách nhiệm nội dung Anh 3 Lang`,
       },
       prism: {
         theme: prismThemes.github,
